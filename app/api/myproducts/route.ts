c995dafc4657bd4fc07d8f76ffc7702cb98f6dfc
winter100 <100taeng@gmail.com>
2023-06-11 23:46:42 +0900

    비밀번호 변경 api준비

--- app/api/myproducts/route.ts
+++ app/api/myproducts/route.ts
@@ -1,10 +1,8 @@
 "use server";
 
-import { ProductsType } from "@/app/page";
 import { connectDatabase } from "@/lib/db";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { NextResponse } from "next/server";
 
 export default async function GetMyProductItems() {
   const collectionName = "Shopping-All-Products";
