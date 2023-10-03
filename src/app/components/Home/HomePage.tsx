01ec5fbcb90c8944480d721906cdd82e7505710b
winter100 <100taeng@gmail.com>
2023-10-03 21:09:52 +0900

    홈페이지 이미지추가, 홈페이지UI변경

--- src/app/components/Home/HomePage.tsx
+++ src/app/components/Home/HomePage.tsx
@@ -1,22 +1,26 @@
 "use clinet";
 import { available } from "@/app/lib/constants-url";
+import Image from "next/image";
 import Link from "next/link";
+import homeImage from "../../../../public/home.jpg";
 
 export default function HomePage() {
   return (
-    <div className="m-auto h-52 text-center  ">
-      <div className=" text-sm grid grid-rows-2 mt-28">
-        <p className=" text-xl ">
-          싹다팜은 내가 가진 물품을 자유롭게 파는 곳 입니다.
-        </p>
-        <div className="m-auto">
-          <Link
-            className="inline-block my-2 w-32 rounded-full border-2 border-primary px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-primary transition duration-150 ease-in-out hover:border-primary-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-primary-600 focus:border-primary-600 focus:text-primary-600 focus:outline-none focus:ring-0 active:border-primary-700 active:text-primary-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-            href={available}
-          >
-            보러가기
-          </Link>
+    <div className="grid m-auto grid-cols-8 h-64 sm:h-96 bg-orange-50 my-12 ">
+      <div className=" col-span-5 m-auto">
+        <div className="h-full">
+          <div className="text-center p-4">
+            <p className=" text-xs sm:text-sm my-8">
+              싹다팜은 내가 가진 물품을 자유롭게 파는 곳 입니다
+            </p>
+            <Link className=" font-bold" href={available}>
+              보러가기
+            </Link>
+          </div>
         </div>
+      </div>
+      <div className=" m-auto col-span-3 relative h-32 w-full sm:h-80">
+        <Image src={homeImage} alt="홈페이지 이미지" fill />
       </div>
     </div>
   );
